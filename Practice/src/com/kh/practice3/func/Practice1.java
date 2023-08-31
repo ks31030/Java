6b7690377f88eda8cbf339a0c929748f5336d734
ks31030 <143372209+ks31030@users.noreply.github.com>
2023-08-31 17:06:33 +0900

    while & for

--- Practice/src/com/kh/practice3/func/Practice1.java
+++ Practice/src/com/kh/practice3/func/Practice1.java
@@ -14,7 +14,7 @@ public class Practice1 {
 		int choice;
 		choice = sc.nextInt();
 		
-		if (choice==1) {
+		/*if (choice==1) {
 			System.out.println("입력메뉴입니다.");
 		} else if (choice==2) {
 			System.out.println("수정메뉴입니다.");
@@ -25,7 +25,26 @@ public class Practice1 {
 		} else if(choice==7){
 			System.out.println("프로그램이 종료됩니다.");
 		}
-		
+		*/
+		switch(choice) {
+			case 1:
+				System.out.println("입력");
+			break;	
+			case 2:
+				System.out.println("수정");
+				break;	
+			case 3:
+				System.out.println("조회");
+				break;	
+			case 4:
+				System.out.println("삭제");
+				break;	
+			case 7:
+				System.out.println("종료");
+				break;	
+			default :
+				System.out.println("잘못 입력된 번호입니다.");
+		}
 	}
 
 }
